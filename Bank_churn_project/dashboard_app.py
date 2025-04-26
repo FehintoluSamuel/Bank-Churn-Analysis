64b6b2bb4e95c1aa29588beed52c3b8438711237
Fehintolu Samuel <fehintoluwasamuel@gmail.com>
2025-04-26 03:04:39 +0100

    Update dashboard_app.py

--- Bank_churn_project/dashboard_app.py
+++ Bank_churn_project/dashboard_app.py
@@ -7,7 +7,7 @@ from sklearn.ensemble import RandomForestClassifier
 st.set_page_config(layout='wide')
 st.title('📊 Bank Churn Full Dashboard')
 # Load cleaned data
-csv_path = r'./data_folder/cleaned_bank_churn_analysis.csv'
+csv_path ='./data_folder/cleaned_bank_churn_analysis.csv'
 df = pd.read_csv(csv_path)
 # ------------------- SECTION 1: Demographics -------------------
 st.header('🌍 Gender & Country Distribution of Customers')
